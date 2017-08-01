e09b3b1b32e7b7b02500d675d949e98b37309106
Jyun-NengJi <jyunnengji@gmail.com>
2017-08-01 21:50:22 +0800

    test file

---
+++ BinaryTree_test.cpp
@@ -0,0 +1,14 @@
+#include "BinaryTree.h"
+
+int main(){
+    BinaryTree<int> BTree;
+    BTree.AddRoot(0);
+    BTree.AddChild(1);
+    BTree.AddChild(2);
+    BTree.AddChild(3);
+    BTree.AddChild(4);
+    BTree.AddChild(5);
+    BTree.PreOrder();
+
+    return 0;
+}
