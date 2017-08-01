92dde7d7702fc5f727532e0b8d00ae9931302634
Jyun-NengJi <jyunnengji@gmail.com>
2017-08-01 21:51:32 +0800

    add TNode class for binary tree

--- Node.h
+++ Node.h
@@ -1,5 +1,8 @@
 #ifndef NODE_H
 #define NODE_H
+#include <cstddef>
+
+using namespace std;
 
 template <class T>
 class Node{
@@ -11,5 +14,24 @@ class Node{
         void addData(T out_data){ data = out_data;}   // add node data.
         void addLink(Node *next_node){ next = next_node;}   // link to next node.
         Node *getNode(){ return next;} // get next node.
+};
+
+template <class T>
+class TNode{
+    private:
+        T data;
+        TNode *left;
+        TNode *right;
+        TNode *parent;
+    public:
+        TNode():parent(NULL), left(NULL), right(NULL), data(){}
+        T getData(){ return data;}
+        TNode *getLeft(){ return left;}
+        TNode *getRight(){ return right;}
+        TNode *getPar(){ return parent;}
+        void addData(T out_data){ data = out_data;}
+        void addRight(TNode *right_node){ right = right_node;}
+        void addLeft(TNode *left_node){ left = left_node;}
+        void addPar(TNode *par_node){ parent = par_node;}
 };
 #endif
